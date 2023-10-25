746ddfcf0418651b433fb927785d77c0b5fa4674
Marichka0701 <tanechnyk.marichka07@gmail.com>
2023-10-25 21:32:07 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Provider} from "react-redux";
+
+import Main from "./components/Main/Main";
+import {store} from "./store/store";
+import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
+import MainLayout from "./layouts/MainLayout/MainLayout";
 
 function App() {
+  const user = localStorage.getItem("token");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Provider store={store}>
+          <Routes>
+              {user && <Route path="/" exact element={<MainLayout />} />}
+              {/*{user && <Route path="/" exact element={<Main />} />}*/}
+              <Route path="/signup" exact element={<Signup />} />
+              <Route path="/login" exact element={<Login />} />
+              <Route path="/" element={<Navigate replace to="/login" />} />
+          </Routes>
+      </Provider>
   );
 }
 
